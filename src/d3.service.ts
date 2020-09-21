1e340d1ab9fc44d0c4a98c6f03b9fb6d4ab8d475
Ladeiras <jtladeiras@gmail.com>
2020-09-21 16:04:55 +0100

    feat(lasso): Added lasso plugin

--- src/d3.service.ts
+++ src/d3.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import * as d3 from './bundle-d3';
-
-export type D3 = typeof d3;
+import { lasso } from 'd3-lasso';
 
 @Injectable()
 export class D3Service {
-
-  constructor() { }
-
-  public getD3(): D3 {
-    return d3;
+  constructor() {
+    (window as any)['d3'] = d3;
   }
 
+  get d3() {
+    return ((window as any)['d3']);
+  }
+
+  get lasso() {
+    return lasso((window as any)['d3']);
+  }
 }
